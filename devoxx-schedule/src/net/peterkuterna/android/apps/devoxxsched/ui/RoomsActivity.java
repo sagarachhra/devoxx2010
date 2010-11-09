951bfe3e9f8623b7a85cff9fa01ce7b387349973
peter.kuterna <peter.kuterna@gmail.com>
2010-11-09 21:38:08 +0000

    Use orange color for counts.

--- devoxx-schedule/src/net/peterkuterna/android/apps/devoxxsched/ui/RoomsActivity.java
+++ devoxx-schedule/src/net/peterkuterna/android/apps/devoxxsched/ui/RoomsActivity.java
@@ -10,6 +10,8 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.LightingColorFilter;
+import android.graphics.drawable.NinePatchDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.BaseColumns;
@@ -106,6 +108,8 @@ public class RoomsActivity extends ListActivity implements AsyncQueryListener {
             descrView.setText("Capacity of " + cursor.getString(RoomsQuery.ROOM_CAPACITY) + " seats.");
             final TextView countView = (TextView) view.findViewById(id.room_count);
             countView.setText(cursor.getString(RoomsQuery.SESSION_COUNT));
+            NinePatchDrawable drawable = (NinePatchDrawable) countView.getBackground();
+            drawable.setColorFilter(new LightingColorFilter(getResources().getColor(R.color.foreground1), 1));
         }
     }
 
