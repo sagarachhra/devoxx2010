d60b99151347c687ee219b33bb048d2380be551d
peter.kuterna <peter.kuterna@b3ecbd42-77ca-42da-36cc-da8a31f22bc6>
2010-11-13 23:03:03 +0000

    Tagging v1.08

---
+++ tags/v1.08/devoxx-schedule/src/net/peterkuterna/android/apps/devoxxsched/Constants.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2010 Peter Kuterna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.peterkuterna.android.apps.devoxxsched;
+
+/**
+ * Class with constants.
+ */
+public class Constants {
+
+	public static final String REST_BASE_URL = "http://cfp.devoxx.com/rest/v1/";
+	public static final String PRESENTATIONS_URL = REST_BASE_URL + "events/1/presentations";
+	public static final String SPEAKERS_URL = REST_BASE_URL + "events/1/speakers";
+	public static final String ROOMS_URL = REST_BASE_URL + "events/1/schedule/rooms";
+	public static final String SCHEDULE_URL = REST_BASE_URL + "events/1/schedule";
+	public static final String MYSCHEDULE_ACTIVATE_URL = REST_BASE_URL + "events/users/activate";
+	public static final String MYSCHEDULE_VALIDATE_URL = REST_BASE_URL + "events/users/validate";
+	public static final String MYSCHEDULE_PUBLISH_URL = REST_BASE_URL + "events/users/publish";
+	public static final String MYSCHEDULE_EMAIL_URL = REST_BASE_URL + "events/users/email";
+	
+	public static final String LABS_BASE_URL = "http://devoxx2010.appspot.com/labs/";
+	public static final String LABS_PRESENTATION_TYPES_URL = LABS_BASE_URL + "presentationtypes.json";
+	public static final String LABS_PRESENTATIONS_URL = LABS_BASE_URL + "cache-labs-presentations.json";
+	public static final String LABS_SPEAKERS_URL = LABS_BASE_URL + "cache-labs-speakers.json";
+	public static final String LABS_SCHEDULE_URL = LABS_BASE_URL + "cache-labs-schedule.json";
+
+}
---
+++ tags/v1.08/devoxx-schedule/src/net/peterkuterna/android/apps/devoxxsched/ui/SessionsOverviewActivity.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2010 Peter Kuterna
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.peterkuterna.android.apps.devoxxsched.ui;
+
+import net.peterkuterna.android.apps.devoxxsched.R;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.Rooms;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.SessionCounts;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.Sessions;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.Tags;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.Tracks;
+import net.peterkuterna.android.apps.devoxxsched.provider.ScheduleContract.Types;
+import net.peterkuterna.android.apps.devoxxsched.util.UIUtils;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+public class SessionsOverviewActivity extends TabActivity {
+
+	private static final String TAG = "SessionsOverviewActivity";
+
+    public static final String TAG_TRACK = "track";
+    public static final String TAG_TYPE = "type";
+    public static final String TAG_TAG = "tag";
+    public static final String TAG_ROOM = "room";
+    public static final String TAG_ALL = "all";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sessions_overview);
+
+        ((TextView) findViewById(R.id.title_text)).setText(getTitle());
+        
+        setupTrackTab();
+        setupTypesTab();
+        setupTagTab();
+        setupRoomTab();
+        setupAllTab();
+    }
+
+    public void onHomeClick(View v) {
+        UIUtils.goHome(this);
+    }
+
+    public void onSearchClick(View v) {
+        UIUtils.goSearch(this);
+    }
+    
+    /** Build and add "track" tab. */
+    private void setupTrackTab() {
+        final TabHost host = getTabHost();
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Tracks.CONTENT_URI);
+        intent.addCategory(Intent.CATEGORY_TAB);
+
+        host.addTab(host.newTabSpec(TAG_TRACK)
+                .setIndicator(buildIndicator(R.string.sessions_track))
+                .setContent(intent));
+    }
+
+    /** Build and add "types" tab. */
+    private void setupTypesTab() {
+        final TabHost host = getTabHost();
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Types.CONTENT_URI);
+        intent.addCategory(Intent.CATEGORY_TAB);
+
+        host.addTab(host.newTabSpec(TAG_TYPE)
+                .setIndicator(buildIndicator(R.string.sessions_type))
+                .setContent(intent));
+    }
+
+    /** Build and add "tags" tab. */
+    private void setupTagTab() {
+        final TabHost host = getTabHost();
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Tags.CONTENT_URI);
+        intent.addCategory(Intent.CATEGORY_TAB);
+
+        host.addTab(host.newTabSpec(TAG_TAG)
+                .setIndicator(buildIndicator(R.string.sessions_tag))
+                .setContent(intent));
+    }
+
+    /** Build and add "rooms" tab. */
+    private void setupRoomTab() {
+        final TabHost host = getTabHost();
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Rooms.CONTENT_URI);
+        intent.addCategory(Intent.CATEGORY_TAB);
+
+        host.addTab(host.newTabSpec(TAG_ROOM)
+                .setIndicator(buildIndicator(R.string.sessions_room))
+                .setContent(intent));
+    }
+
+    /** Build and add "all" tab. */
+    private void setupAllTab() {
+        final TabHost host = getTabHost();
+
+        final Uri uri = Sessions.CONTENT_URI
+        	.buildUpon()
+        	.appendQueryParameter(SessionCounts.SESSION_INDEX_EXTRAS, Boolean.TRUE.toString())
+        	.build();
+        final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addCategory(Intent.CATEGORY_TAB);
+        intent.putExtra(SessionsActivity.EXTRA_FOCUS_CURRENT_NEXT_SESSION, true);
+
+        host.addTab(host.newTabSpec(TAG_ALL)
+                .setIndicator(buildIndicator(R.string.sessions_all))
+                .setContent(intent));
+    }
+    
+    /**
+     * Build a {@link View} to be used as a tab indicator, setting the requested
+     * string resource as its label.
+     */
+    private View buildIndicator(int textRes) {
+        final TextView indicator = (TextView) getLayoutInflater().inflate(R.layout.tab_indicator,
+                getTabWidget(), false);
+        indicator.setText(textRes);
+        return indicator;
+    }
+    
+}
